e01c950469030862746d4120b5459d49f8fb1c77
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-07-10 14:10:57 -0300

    feat: create room, style room and home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
-import { Room } from './pages/rooms'
+import { Room } from './pages/room'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +11,7 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room/:id" />
+          <Route element={<Room />} path="/room/:roomId" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './app.tsx'
+import { App } from './app'
 
 // biome-ignore lint/style/noNonNullAssertion: mandatory by React
 createRoot(document.getElementById('root')!).render(
